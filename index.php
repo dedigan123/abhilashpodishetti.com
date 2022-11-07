342ba35f5c44ea15ef0e78f837d1fbc2a49992b6
dedigan123 <117627118+dedigan123@users.noreply.github.com>
2022-11-07 14:07:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<link rel="Shortcut Icon" href="https://g.top4top.io/p_2493wkilz1.jpg" type="image/x-icon"></link>
+<head>
+<title>Hacked By White System'./404</title>
+</head>
+<body text="#FFFFFF" bgcolor="#000000">
+<style>
+
+body {
+  background-image: url('https://i.gifer.com/origin/8a/8a4b5e12f3abed35b73c528c02ad5dd0.gif');
+  background-attachment: fixed;
+  background-size:100% 100%;
+}
+</style>
+<center>
+<img src="https://g.top4top.io/p_2493wkilz1.jpg" height="300"  width="400">
+<br><br>
+<font face="futurism"><h1>Hacked By White System'./404</font></h1>
+<font face="aquire"><h3> I LOVE YOU EVERDAY </font></h3>
+<font face="aquire"><h4>Greetz : /Rayzky_ - Mr.Onion  - RadicalHaxor - evil-net </font></h4>
+
+<iframe width="0" height="0" src="https://www.youtube.com/embed/iJhPZzm6jhg?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</center>
+</body>
+
+</html>
